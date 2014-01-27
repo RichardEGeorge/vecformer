90e2687a528b305504e2567c05d9b7097858759f
Richard George <rg.george1@gmail.com>
2014-01-27 00:01:57 +0000

    revise polygon test unit

--- test_poly.py
+++ test_poly.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 
 from Polygon_2 import *;
 
@@ -8,7 +9,7 @@ p1.add_vertex(10,0);
 p1.add_vertex(10,10);
 p1.add_vertex(0,10);
 
-p2 = Polygon_2("A");
+p2 = Polygon_2("B");
 
 p2.add_vertex(5,5);
 p2.add_vertex(15,5);
@@ -17,22 +18,31 @@ p2.add_vertex(5,15);
 
 yn = lambda b: "yes" if b else "no";
 
-print "Is polygon simple? %s" % yn(p1.is_simple());
-print "Is polygon convex? %s" % yn(p1.is_convex());
-print "Polygon %s has signed area : %g" % (p1.name,p1.area());
+def describe_poly(p):
+	print "Is polygon %s simple? %s" % (p.name,yn(p.is_simple()));
+	print "Is polygon %s convex? %s" % (p.name,yn(p.is_convex()));
+	print "Polygon %s has signed area : %g" % (p.name,p.area());
+	print "Polygon %s has vertices :\n%s" % (p.name,p.get_vertices());
 
 def test_inside(p,x,y):
 	result = p.is_inside(x,y);
-	print "Is the point (%g,%g) inside polygon %s? %d" % (x,y,p.name,result);
-	return result;
+	print "Is the point (%g,%g) inside polygon %s? : %s" % (x,y,p.name,yn(result));
 
-test_inside(p1,5.0,5.0);
-test_inside(p1,20.0,20.0);
+describe_poly(p1);
 
-print p1.get_vertices();
+test_inside(p1,4.0,4.0);
+test_inside(p1,12.0,12.0);
+
+describe_poly(p2);
+
+test_inside(p2,4.0,4.0);
+test_inside(p2,12.0,12.0);
 
 print "union of p1 and p2:"
 
 p3 = Polygon_2.as_union(p1,p2);
 
-print p3.get_vertices();
+describe_poly(p3);
+
+test_inside(p3,4.0,4.0);
+test_inside(p3,12.0,12.0);
